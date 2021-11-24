ba75b2cc938e5b9129035cea46a194273b00c549
Tobias Olsen <olsen@iraqbabbler.stats.ox.ac.uk>
2021-11-24 15:49:26 +0000

    added to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+    
 setup(
     name='ablang',
     version='1.0.1',
@@ -13,7 +14,7 @@ setup(
     long_description_content_type='text/markdown',
     maintainer_email='tobias.olsen@stats.ox.ac.uk',
     include_package_data=True,
-    packages=find_packages(include=('ablang')),
+    packages=find_packages(include=('ablang', 'ablang.*')),
     install_requires=[
         'numpy',
         'fairseq',
